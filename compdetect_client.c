968025bd29a5e90ab09f95843806299a91033da1
Yifan Meng <ymeng20@vlab06.cs.usfca.edu>
2023-03-22 22:50:40 -0700

    Implement UDP packet trains

--- compdetect_client.c
+++ compdetect_client.c
@@ -4,52 +4,15 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <time.h>
 
 #include "compdetect_config_parser.h"
-
-int send_packets (int sock, char *buffer, int buffer_len)
-{
-    int sent_bytes = send (sock, buffer, buffer_len, 0);
-
-    if (sent_bytes < 0)
-        perror ("send() failed");
-        
-    return 0;
-}
-
-int receive_packets (int sock, char *buffer, int buffer_len)
-{
-    int num_received = recv(sock, buffer, buffer_len, 0);
-
-    if (num_received < 0)
-        perror ("recv() failed");
-
-    else if (num_received == 0)
-        return EOF;
-
-    else
-        return num_received;
-}
-
-int establish_connection (int sock, const char *server_addr, int server_port)
-{
-    struct sockaddr_in sin;
-    sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = inet_addr (server_addr);
-    sin.sin_port = htons (server_port);
-
-    if (connect (sock, (struct sockaddr *) &sin, sizeof (sin)) < 0)
-    {
-        perror ("cannot connect to server");
-        return -1;
-    }
-    return 0;
-}
 
 char *read_file (const char *filename, int *file_size)
 {
@@ -67,12 +30,12 @@ char *read_file (const char *filename, int *file_size)
     char *buffer = malloc (*file_size);
     if (!buffer)
     {
-        perror ("cannot allocate memory");
+        perror ("cannot allocate memory for file data");
         close (fd);
         return NULL;
     }
 
-    if (read (fd, buffer, file_size) != file_size)
+    if (read (fd, buffer, *file_size) != *file_size)
     {
         perror ("cannot read file");
         close (fd);
@@ -84,6 +47,88 @@ char *read_file (const char *filename, int *file_size)
     return buffer;
 }
 
+int setup_tcp_connection (int sock, const char *server_addr, int server_port)
+{
+    struct sockaddr_in sin;
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = inet_addr (server_addr);
+    sin.sin_port = htons (server_port);
+
+    if (connect (sock, (struct sockaddr *) &sin, sizeof (sin)) < 0)
+    {
+        perror ("cannot connect to server");
+        return -1;
+    }
+    return 0;
+}
+
+int send_tcp_packet (int sock, const char *buffer, int buffer_size)
+{
+    int bytes_sent = send (sock, buffer, buffer_size, 0);
+
+    if (bytes_sent < 0)
+    {
+        perror ("send () failed");
+    }
+        
+    return 0;
+}
+
+int receive_tcp_packet (int sock, char *buffer, int buffer_size)
+{
+    int bytes_received = recv(sock, buffer, buffer_size, 0);
+
+    if (bytes_received < 0)
+    {
+        perror ("recv () failed");
+    }
+    else if (bytes_received == 0)
+    {
+        return EOF;
+    }
+
+    return bytes_received;
+}
+
+int setup_udp_socket (int port)
+{
+    int sock;
+    struct sockaddr_in sin;
+
+    if ((sock = socket (AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror ("cannot create UDP socket");
+        return -1;
+    }
+
+    memset (&sin, 0, sizeof (sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons (port);
+
+    if (bind (sock, (struct sockaddr *) &sin, sizeof (sin)) < 0)
+    {
+        perror ("cannot bind UDP socket to address");
+        close (sock);
+        return -1;
+    }
+
+    return sock;
+}
+
+int send_udp_packet (int sock, const char *buffer, int buffer_size, struct sockaddr_in *dst_addr)
+{
+    int bytes_sent;
+
+    if ((bytes_sent = sendto (sock, buffer, buffer_size, 0, (struct sockaddr *) dst_addr, sizeof (*dst_addr))) < 0)
+    {
+        perror ("sendto () failed");
+        return -1;
+    }
+
+    return bytes_sent;
+}
+
 int main (int argc, char const *argv[])
 {
     if (argc < 2)
@@ -91,60 +136,123 @@ int main (int argc, char const *argv[])
         printf ("Usage: %s <json_config_file>\n", argv[0]);
         exit (EXIT_FAILURE);
     }
-    
-    int fd = open (argv[1], O_RDONLY);
-    if (fd < 0)
-    {
-        perror ("cannot open file");
-        exit (EXIT_FAILURE);
-    }
 
-    struct stat st;
-    if (fstat (fd, &st) < 0)
-    {
-        perror ("cannot get file information");
-        close (fd);
-        exit (EXIT_FAILURE);
-    }
-
-    int file_size;
-    char *buffer = read_file (argv[1], &file_size);
-    if (!buffer)
+    int config_buf_size;
+    char *config_buf = read_file (argv[1], &config_buf_size);
+    if (!config_buf)
     {
         exit (EXIT_FAILURE);
     }
 
-    struct compdetect_config *config = parse_json_string (buffer);
+    struct compdetect_config *config = parse_json_string (config_buf);
 
-    int sock = socket (AF_INET, SOCK_STREAM, 0);
-    if (sock < 0)
+    int tcp_sock = socket (AF_INET, SOCK_STREAM, 0);
+    if (tcp_sock < 0)
     {
         perror ("cannot create TCP socket");
-        free (buffer);
         free (config);
+        free (config_buf);
         exit (EXIT_FAILURE);
     }
 
-    if (establish_connection (sock, config->server_ip_addr, config->tcp_dest_port) < 0)
+    if (setup_tcp_connection (tcp_sock, config->server_ip_addr, config->tcp_dest_port) < 0)
     {
-        close (sock);
-        free (buffer);
+        close (tcp_sock);
         free (config);
+        free (config_buf);
         exit (EXIT_FAILURE);
     }
 
-    if (send_packets (sock, buffer, file_size) < 0)
+    if (send_tcp_packet (tcp_sock, config_buf, config_buf_size) < 0)
     {
-        perror ("cannot send data to server");
-        close (sock);
-        free (buffer);
+        perror ("cannot send config data to server");
+        close (tcp_sock);
+        free (config);
+        free (config_buf);
+        exit (EXIT_FAILURE);
+    }
+
+    free (config_buf);
+    close (tcp_sock);
+    sleep (1);
+
+    int udp_sock = setup_udp_socket (config->udp_src_port);
+    if (udp_sock < 0)
+    {
         free (config);
         exit (EXIT_FAILURE);
     }
 
-    close (sock);
-    free (buffer);
+    int enable = IP_PMTUDISC_DO;
+    if (setsockopt (udp_sock, IPPROTO_IP, IP_MTU_DISCOVER, &enable, sizeof (enable)) < 0)
+    {
+        perror ("setsockopt () failed");
+        close (udp_sock);
+        free (config);
+        exit (EXIT_FAILURE);
+    }
+
+    struct sockaddr_in dst_addr;
+    memset (&dst_addr, 0, sizeof (dst_addr));
+    dst_addr.sin_family = AF_INET;
+    dst_addr.sin_addr.s_addr = inet_addr (config->server_ip_addr);
+    dst_addr.sin_port = htons (config->udp_dest_port);
+
+    int random_buf_size;
+    char *random_buf = read_file ("random_file", &random_buf_size);
+    if (!random_buf)
+    {
+        close (udp_sock);
+        free (config);
+        exit (EXIT_FAILURE);
+    }
+
+    for (int train = 0; train < 2; train++)
+    {
+        for (int packet_id = 0; packet_id < config->udp_packet_count; packet_id++)
+        {
+            char *udp_buf = malloc (config->udp_payload_size);
+            if (!udp_buf)
+            {
+                perror ("cannot allocate memory for UDP packet");
+                close (udp_sock);
+                free (config);
+                free (random_buf);
+                exit (EXIT_FAILURE);
+            }
+
+            memset (udp_buf, 0, config->udp_payload_size);
+
+            uint16_t packet_id_net = htons ((uint16_t) packet_id);
+            memcpy (udp_buf, &packet_id_net, sizeof (packet_id_net));
+
+            if (train == 1)
+            {
+                memcpy (udp_buf + sizeof (packet_id_net), random_buf, config->udp_payload_size - sizeof (packet_id_net));
+            }
+
+            if (send_udp_packet (udp_sock, udp_buf, config->udp_payload_size, &dst_addr) < 0)
+            {
+                perror ("cannot send UDP packet");
+                close (udp_sock);
+                free (config);
+                free (random_buf);
+                free (udp_buf);
+                exit (EXIT_FAILURE);
+            }
+
+            free (udp_buf);
+        }
+
+        if (train == 0)
+        {
+            sleep (config->inter_measurement_time);
+        }
+    }
+
+    close (udp_sock);
     free (config);
+    free (random_buf);
 
     return 0;
 }
--- compdetect_server.c
+++ compdetect_server.c
@@ -4,85 +4,143 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <sys/time.h>
+#include <sys/select.h>
 
-int send_packets (int sock, char *buffer, int buffer_len)
+#include "compdetect_config_parser.h"
+
+int setup_tcp_socket (int port)
 {
-    int sent_bytes = send (sock, buffer, buffer_len, 0);
+    int sock;
+    int optval = 1;
+    struct sockaddr_in sin;
 
-    if (sent_bytes < 0)
-        perror ("send() failed");
-        
-    return 0;
-}
-
-int receive_packets (int sock, char *buffer, int buffer_len)
-{
-    int num_received = recv(sock, buffer, buffer_len, 0);
-
-    if (num_received < 0)
-        perror ("recv() failed");
-
-    else if (num_received == 0)
-        return EOF;
-
-    else
-        return num_received;
-}
-
-int setup_socket ()
-{
-    int sock = socket (AF_INET, SOCK_STREAM, 0);
-    if (sock < 0)
+    if ((sock = socket (AF_INET, SOCK_STREAM, 0)) < 0)
     {
         perror ("cannot create TCP socket");
         return -1;
     }
-    return sock;
-}
-
-int bind_and_listen (int sock, int server_port)
-{
-    int optval = 1;
-    struct sockaddr_in sin;
 
     if (setsockopt (sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0)
     {
         perror ("cannot reuse address");
+        close (sock);
         return -1;
     }
 
     memset (&sin, 0, sizeof(sin));
-
     sin.sin_family = AF_INET;
     sin.sin_addr.s_addr = INADDR_ANY;
-    sin.sin_port = htons (server_port);
+    sin.sin_port = htons (port);
 
     if (bind (sock, (struct sockaddr *) &sin, sizeof (sin)) < 0)
     {
-        perror ("cannot bind socket to address");
+        perror ("cannot bind TCP socket to address");
+        close (sock);
         return -1;
     }
 
     if (listen (sock, 5) < 0)
     {
         perror ("error listening");
+        close (sock);
         return -1;
     }
 
-    return 0;
+    return sock;
 }
 
-int accept_connection (int sock)
+int accept_tcp_connection (int sock)
 {
     struct sockaddr_in addr;
-    int addr_len = sizeof (addr);
-    int client_sock = accept (sock, (struct sockaddr *) &addr, &addr_len);
+    int addr_size = sizeof (addr);
+    int client_sock = accept (sock, (struct sockaddr *) &addr, &addr_size);
     if (client_sock < 0)
     {
         perror ("error accepting connection");
+        close (sock);
         return -1;
     }
     return client_sock;
+}
+
+int send_tcp_packet (int sock, const char *buffer, int buffer_size)
+{
+    int bytes_sent = send (sock, buffer, buffer_size, 0);
+
+    if (bytes_sent < 0)
+    {
+        perror ("send () failed");
+    }
+        
+    return 0;
+}
+
+int receive_tcp_packet (int sock, char *buffer, int buffer_size)
+{
+    int bytes_received = recv(sock, buffer, buffer_size, 0);
+
+    if (bytes_received < 0)
+    {
+        perror ("recv () failed");
+    }
+    else if (bytes_received == 0)
+    {
+        return EOF;
+    }
+        
+    return bytes_received;
+}
+
+int setup_udp_socket (int port)
+{
+    int sock;
+    int optval = 1;
+    struct sockaddr_in sin;
+
+    if ((sock = socket (AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror ("cannot create UDP socket");
+        return -1;
+    }
+
+    if (setsockopt (sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0)
+    {
+        perror ("cannot reuse address");
+        close (sock);
+        return -1;
+    }
+
+    memset (&sin, 0, sizeof (sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons (port);
+
+    if (bind (sock, (struct sockaddr *) &sin, sizeof (sin)) < 0)
+    {
+        perror ("cannot bind UDP socket to address");
+        close (sock);
+        return -1;
+    }
+
+    return sock;
+}
+
+int receive_udp_packet (int sock, char *buffer, int buffer_size, struct sockaddr_in *src_addr)
+{
+    socklen_t addr_size = sizeof (*src_addr);
+    int bytes_received = recvfrom (sock, buffer, buffer_size, 0, (struct sockaddr *) src_addr, &addr_size);
+
+    if (bytes_received < 0)
+    {
+        perror ("recvfrom () failed");
+    }
+    else if (bytes_received == 0)
+    {
+        return EOF;
+    }
+
+    return bytes_received;
 }
 
 int main (int argc, char const *argv[])
@@ -93,56 +151,146 @@ int main (int argc, char const *argv[])
         exit (EXIT_FAILURE);
     }
 
-    int server_port = atoi (argv[1]);
-    if (server_port == 0)
+    int tcp_port = atoi (argv[1]);
+    if (tcp_port == 0)
     {
         perror ("invalid TCP port number");
         exit (EXIT_FAILURE);
     }
 
-    int sock = setup_socket ();
-    if (sock < 0)
+    int tcp_sock = setup_tcp_socket (tcp_port);
+    if (tcp_sock < 0)
     {
         exit (EXIT_FAILURE);
     }
 
-    if (bind_and_listen (sock, server_port) < 0)
-    {
-        close (sock);
-        exit (EXIT_FAILURE);
-    }
-
-    int client_sock = accept_connection (sock);
+    int client_sock = accept_tcp_connection (tcp_sock);
     if (client_sock < 0)
     {
-        close (sock);
         exit (EXIT_FAILURE);
     }
 
-    int buffer_size = 4096;
-    char *buffer = malloc (buffer_size);
-    if (!buffer)
+    int buffer_size = 1024;
+    char *config_buf = malloc (buffer_size);
+    if (!config_buf)
     {
-        perror ("cannot allocate memory");
-        close (sock);
+        perror ("cannot allocate memory for config data");
+        close (tcp_sock);
         close (client_sock);
         exit (EXIT_FAILURE);
     }
 
-    if (receive_packets (client_sock, buffer, buffer_size) < 0)
+    if (receive_tcp_packet (client_sock, config_buf, buffer_size) < 0)
     {
-        perror ("error receiving data");
-        close (sock);
+        perror ("cannot receive config data from client");
+        close (tcp_sock);
         close (client_sock);
-        free (buffer);
+        free (config_buf);
         exit (EXIT_FAILURE);
     }
 
-    printf ("Received config data:\n%s\n", buffer);
+    struct compdetect_config *config = parse_json_string (config_buf);
+    if (!config)
+    {
+        perror ("error parsing configuration");
+        close (tcp_sock);
+        close (client_sock);
+        free (config_buf);
+        exit (EXIT_FAILURE);
+    }
 
-    close (sock);
     close (client_sock);
-    free (buffer);
+    free (config_buf);
+
+    int udp_sock = setup_udp_socket (config->udp_dest_port);
+    if (udp_sock < 0)
+    {
+        close (tcp_sock);
+        free (config);
+        exit (EXIT_FAILURE);
+    }
+    
+    char *udp_buf = malloc (config->udp_payload_size);
+    if (!udp_buf)
+    {
+        perror ("cannot allocate memory for low entropy data");
+        close (tcp_sock);
+        free (config);
+        exit (EXIT_FAILURE);
+    }
+
+    struct sockaddr_in src_addr;
+    int train_index = 0;
+    
+    struct timeval first_packet_time[2], last_packet_time[2];
+    struct timeval now;
+
+    memset(first_packet_time, 0, sizeof(first_packet_time));
+    memset(last_packet_time, 0, sizeof(last_packet_time));
+    memset(&now, 0, sizeof(now));
+
+    fd_set readfds;
+    struct timeval select_timeout;
+
+    while (train_index < 2)
+    {
+        FD_ZERO (&readfds);
+        FD_SET (udp_sock, &readfds);
+
+        select_timeout.tv_sec = 10;
+        select_timeout.tv_usec = 0;
+
+        int activity = select (udp_sock + 1, &readfds, NULL, NULL, &select_timeout);
+
+        if (activity < 0)
+        {
+            perror ("select () failed");
+            break;
+        }
+        else if (activity == 0)
+        {
+            train_index++;
+            continue;
+        }
+
+        int num_received = receive_udp_packet (udp_sock, udp_buf, config->udp_payload_size, &src_addr);
+
+        if (num_received > 0)
+        {
+            gettimeofday (&now, NULL);
+
+            uint16_t packet_id = ntohs(*(uint16_t *) udp_buf);
+            printf("Packet ID: %u\n", packet_id);
+
+            if (packet_id < config->udp_packet_count)
+            {
+                if (!first_packet_time[train_index].tv_sec)
+                {
+                    first_packet_time[train_index] = now;
+                }
+                last_packet_time[train_index] = now;
+            }
+        }
+        else
+        {
+            perror ("cannot receive UDP packet");
+            break;
+        }
+    }
+
+    close (udp_sock);
+    free (udp_buf);
+
+    double first_train_duration = ((last_packet_time[0].tv_sec - first_packet_time[0].tv_sec) * 1000.0
+                                    + (last_packet_time[0].tv_usec - first_packet_time[0].tv_usec) / 1000.0);
+    double second_train_duration = ((last_packet_time[1].tv_sec - first_packet_time[1].tv_sec) * 1000.0
+                                    + (last_packet_time[1].tv_usec - first_packet_time[1].tv_usec) / 1000.0);
+
+    printf ("Train 1 arrival time difference: %.2lf ms\n", first_train_duration);
+    printf ("Train 2 arrival time difference: %.2lf ms\n", second_train_duration);
+
+    close (tcp_sock);
+    free (config);
     
     return 0;
 }
